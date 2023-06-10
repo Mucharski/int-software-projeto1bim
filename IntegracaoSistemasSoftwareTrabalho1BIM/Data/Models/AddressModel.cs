cfc36a012e423e358408dce453a6066e0a544545
Igor Mucharski <igor.m99@hotmail.com>
2023-06-10 09:34:43 -0300

    fixing AddressModel

--- IntegracaoSistemasSoftwareTrabalho1BIM/Data/Models/AddressModel.cs
+++ IntegracaoSistemasSoftwareTrabalho1BIM/Data/Models/AddressModel.cs
@@ -1,8 +1,12 @@
-﻿namespace IntegracaoSistemasSoftwareTrabalho1BIM.Data.Models;
+﻿using Newtonsoft.Json;
+
+namespace IntegracaoSistemasSoftwareTrabalho1BIM.Data.Models;
 
 public class AddressModel
 {
     public int Id { get; set; }
+    
+    [JsonIgnore]
     public RestaurantModel Restaurant { get; set; }
     public int RestaurantId { get; set; }
     public string Address { get; set; }
